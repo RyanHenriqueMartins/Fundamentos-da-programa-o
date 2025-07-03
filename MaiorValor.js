997924593fb0b2618b64638938c40a668739386e
RyanHenriqueMartins <110641688+RyanHenriqueMartins@users.noreply.github.com>
2025-07-02 21:01:33 -0300

    JS de vetores

--- MaiorValor.js
+++ MaiorValor.js
@@ -1,4 +1,4 @@
-function ObtemMaiorValor(vet) {
+function ObtemMaiorValor() {
     if (vet.length === 0) {
         return null;//* Para eu não esquecer,null é um valor especial usado para indicar que uma variável não se refere a nenhum objeto ou valor válido. *//
     }
@@ -11,4 +11,48 @@ function ObtemMaiorValor(vet) {
         }
     }
     return maior;
+}
+
+function ObtemMenorValor() {
+
+    let menor = vet[0];
+
+    for (let i = 0; i < vet.length; i++) {
+        if (vet[i] < menor) {
+            menor = vet[i];
+        }
+    }
+    return menor;
+}
+
+function ObtemSomaValor() {
+    let soma = 0;
+    for (let i = 0; i < vet.length; i++) {
+
+        soma += vet[i];
+    }
+    return soma;
+}
+
+function ObtemMediaValor() {
+
+    let vasco = ObtemSomaValor()
+    let media = vasco / vet.length
+
+    return media;
+}
+
+function SomaMatrizes(matA, matB) {
+    let soma1 = 0;
+    let soma2 = 0;
+
+    for (let i = 0; i < matA.lenght; i++) {
+
+        soma1 += matA[i];
+
+    }
+    let somaMatrizes = soma1 + soma2;
+    return somaMatrizes;
+
+
 }
